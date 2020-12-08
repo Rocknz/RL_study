b328d4b834b93e406d75390386c0069627bf6ce6
Rocknz <jrj325@hanmir.com>
2020-12-08 21:14:07 +0900

    update actor critic multithreading

--- actor_critic/cartpole/actor-critic.py
+++ actor_critic/cartpole/actor-critic.py
@@ -2,31 +2,32 @@ import torch
 from torch import nn
 import gym
 import numpy as np
-
+import random
+from threading import Thread, Lock
 # independent.
 
 # Critic
 # Q^PI (s, a), PI(s) = [values] ==> #a
 
-# PI(s) = vector_a(a == argmax_a Q^PI (s, a)) +
+# PI(s) = vector_a(a == argmax_a Q^PI (s, aa)) +
 # PI has soft-max
 
 
 class Actor(nn.Module):
-    def Actor(self, state_space, action_space):
+    def __init__(self, state_space, action_space):
+        super(Actor, self).__init__()
         self.l1 = nn.Linear(state_space, 1000)
         self.l = nn.Sequential(
-            [
-                nn.LeakyReLU(),
-                nn.Linear(1000, 1000),
-                nn.LeakyReLU(),
-                nn.Linear(1000, 100),
-                nn.LeakyReLU(),
-                nn.Linear(100, 100),
-                nn.LeakyReLU(),
-                nn.Linear(100, action_space),
-                nn.Softmax(),
-            ]
+            nn.LeakyReLU(),
+            nn.Linear(1000, 1000),
+            nn.LeakyReLU(),
+            nn.Linear(1000, 100),
+            nn.LeakyReLU(),
+            nn.Linear(100, 100),
+            nn.LeakyReLU(),
+            nn.Linear(100, action_space),
+            nn.LeakyReLU(),
+            nn.Softmax(1)
         )
 
     def forward(self, x):
@@ -36,19 +37,18 @@ class Actor(nn.Module):
 
 
 class Critic(nn.Module):
-    def Critic(self, state_space, action_space):
-        self.l1 = nn.Linear(state_space + action_space, 100)
+    def __init__(self, state_space, action_space):
+        super(Critic, self).__init__()
+        self.l1 = nn.Linear(state_space, 1000)
         self.l = nn.Sequential(
-            [
-                nn.LeakyReLU(),
-                nn.Linear(1000, 1000),
-                nn.LeakyReLU(),
-                nn.Linear(1000, 100),
-                nn.LeakyReLU(),
-                nn.Linear(100, 100),
-                nn.LeakyReLU(),
-                nn.Linear(100, 1),
-            ]
+            nn.LeakyReLU(),
+            nn.Linear(1000, 1000),
+            nn.LeakyReLU(),
+            nn.Linear(1000, 100),
+            nn.LeakyReLU(),
+            nn.Linear(100, 100),
+            nn.LeakyReLU(),
+            nn.Linear(100, action_space)
         )
 
     def forward(self, x):
@@ -58,9 +58,19 @@ class Critic(nn.Module):
 
 
 class Learning:
-    def Learning(self, state_space, action_space):
-        self.actor = Actor(state_space, action_space)
-        self.critic = Critic(state_space, action_space)
+    def __init__(self, state_space, action_space, cuda_num=0):
+        self.actor = Actor(state_space, action_space).cuda(cuda_num)
+        self.critic = Critic(state_space, action_space).cuda(cuda_num)
+        self.cuda_num = cuda_num
+        self.sample_size = 1000
+        self.criterion = nn.MSELoss(reduction='sum')
+        self.gamma = 0.95
+        self.learning_rate = 0.001
+        self.optimizer_actor = torch.optim.Adam(
+            params=self.actor.parameters(), lr=self.learning_rate)
+
+        self.optimizer_critic = torch.optim.Adam(
+            params=self.critic.parameters(), lr=self.learning_rate)
 
     def save(self):
         torch.save(self.actor.state_dict(), "cartpole_actor")
@@ -75,15 +85,108 @@ class Learning:
             print("load_error!")
 
     def action(self, state):
-        res = self.actor(state).numpy()
-        val = np.random.choice(5, 3, p=res)
+        torch_state = torch.from_numpy(
+            state).float().unsqueeze(0).cuda(self.cuda_num)
+        res = self.actor(torch_state).squeeze(0).cpu().detach().numpy()
+        val = np.random.choice(2, 1, p=res)[0]
         return val
 
-    def learn_critic(self, history):
-        pass
+    def sampling(self, history):
+        size = self.sample_size
+        if size > len(history):
+            size = len(history)
+
+        history_sample = random.sample(history, size)
+        return history_sample
 
     def learn_actor(self, history):
-        pass
+        sample = self.sampling(history)
+        s = np.array([i[0] for i in sample])
+        # action = np.array([i[1] for i in sample])
+        s = torch.from_numpy(s).float().cuda(self.cuda_num)
+        now = self.actor(s)
+        with torch.no_grad():
+            n_now = self.critic(s)
+
+        softmax = nn.Softmax(1)
+        n_now = softmax(n_now)
+
+        self.optimizer_actor.zero_grad()
+        loss = self.criterion(now, n_now)
+        loss.backward()
+        self.optimizer_actor.step()
+
+    def learn_critic(self, history):
+        sample = self.sampling(history)
+        s = np.array([i[0] for i in sample])
+        action = np.array([i[1] for i in sample])
+        s_next = np.array([i[2] for i in sample])
+        action_next = np.array([i[3] for i in sample])
+        # action = np.array([i[1] for i in sample])
+        reward = np.array([i[4] for i in sample])
+        done = np.array([i[5] for i in sample])
+
+        s = torch.from_numpy(s).float().cuda(self.cuda_num)
+        action = torch.from_numpy(action).long().cuda(self.cuda_num)
+        s_next = torch.from_numpy(s_next).float().cuda(self.cuda_num)
+        action_next = torch.from_numpy(action_next).long().cuda(self.cuda_num)
+
+        # action = torch.from_numpy(action).float().cuda(self.cuda_num)
+        reward = torch.from_numpy(reward).float().cuda(self.cuda_num)
+        done = torch.from_numpy(done).bool().cuda(self.cuda_num)
+
+        n_now = self.critic(s)
+        n_next = self.critic(s_next)
+
+        iters = torch.Tensor(range(n_now.shape[0])
+                             ).long().cuda(self.cuda_num)
+
+        n_now = n_now[iters[:], action[:]]
+        n_next = n_next[iters[:], action_next[:]]
+
+        ans = reward + self.gamma * n_next
+        # if its done. => Q-value = 0
+        ans[done] = 0
+
+        self.optimizer_critic.zero_grad()
+        loss = self.criterion(n_now, ans)
+        loss.backward()
+        self.optimizer_critic.step()
+        return loss.item() / len(sample)
+
+
+def simulation(index, mutex, history, steps, learning):
+    env = gym.make("CartPole-v1")
+    state = env.reset()
+    step = 0
+    next_action = learning.action(state)
+    while True:
+        # env.render()
+        action = next_action
+
+        before_state = state
+        before_action = action
+        state, reward, done, info = env.step(action)
+        next_action = learning.action(state)
+        if state[0] < 0.5 and state[0] > -0.5:
+            reward += (0.5 - abs(state[0])) * (0.5 - abs(state[0])) * 4
+
+        if state[1] < 0.5 and state[1] > -0.5:
+            reward += (0.5 - abs(state[1])) * (0.5 - abs(state[1])) * 4
+        
+        reward *= 20
+        
+        mutex.acquire()
+        history.append(
+            [before_state, before_action, state, next_action, reward, done])
+        mutex.release()
+
+        step += 1
+
+        if done:
+            break
+
+    steps[index] = step
 
 
 def main():
@@ -103,27 +206,49 @@ def main():
 
         avg_step = 0
         N = 0
-        for _ in range(100):
-            state = env.reset()
-            step = 0
-            while True:
-                action = learning.action(state)
-                before_state = state
-                before_action = action
-                state, reward, done, info = env.step(action)
-                history.append([before_state, before_action, state, reward, done])
-                learning.learn_actor(history)
-                step += 1
 
-                if done:
-                    break
+        # run simulation with multithread
+        mutex = Lock()
+        threads = []
+        steps = []
+        simulation_num = 500
+        print("sim_start")
+        for _ in range(simulation_num):
+            steps.append(0)
+
+        for index in range(simulation_num):
+            step = 0
+            thread = Thread(target=simulation, args=(
+                index, mutex, history, steps, learning))
+            thread.start()
+            threads.append(thread)
+
+        for thread in threads:
+            thread.join()
+
+        for step in steps:
             avg_step += step
             N += 1
 
         avg_step /= N
         print("avg_step {}".format(avg_step))
+        print("learn_critic")
+        min = -1
+        # while True:
         for _ in range(1000):
-            learning.learn_critic(history)
+            loss = learning.learn_critic(history)
+            # if loss < min or min == -1:
+            #     min = loss
+            #     print(min)
+
+            # if loss < 0.1:
+            #     break
+
+        print("learn_actor")
+        for _ in range(1000):
+            learning.learn_actor(history)
+
+        learning.save()
 
 
 if __name__ == "__main__":
