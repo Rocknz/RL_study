5710fb72f168191f902c060e8ebbdff8992e17ea
Rakjoon Chung <jrj325@hanmir.com>
2020-12-19 22:21:19 +0900

    Multi thread mode

--- LunarLanderContinuous/main.py
+++ LunarLanderContinuous/main.py
@@ -3,32 +3,32 @@ from Reinforcement.ActorCritic import ActorCritic
 import numpy
 import gym
 import torch
+from threading import Thread, Lock
 
+def simulation(index, mutex, history, steps, RL):
+    env = gym.make("LunarLanderContinuous-v2")
+    state = env.reset()
+    history = []
+    action = RL.action(state)
+    step = 0
+    while True:
+        # print(state)
+        # env.render()
 
-# def simulation(index, mutex, history, steps, learning):
-#     env = gym.make("CartPole-v1")
-#     state = env.reset()
-#     step = 0
-#     next_action = learning.action(state)
-#     while True:
-#         # env.render()
-#         action = next_action
+        before_state = state
+        before_action = action
 
-#         before_state = state
-#         before_action = action
-#         state, reward, done, info = env.step(action)
-#         next_action = learning.action(state)
+        state, reward, done, info = env.step(action)
+        action = RL.action(state)
+        mutex.acquire()
+        history.append([before_state, before_action, state, action, reward, done])
+        mutex.release()
 
-#         mutex.acquire()
-#         history.append([before_state, before_action, state, next_action, reward, done])
-#         mutex.release()
+        step += 1
+        if done:
+            break
 
-#         step += 1
-
-#         if done:
-#             break
-
-#     steps[index] = step
+    steps[index] = step
 
 def main():
     torch.device("cuda:0")
@@ -51,27 +51,35 @@ def main():
     # main engine, left - mid - right engine
     print("action_space", env.action_space)
     RL = ActorCritic(name = "Lunar", state_space = state_space, action_space = action_space)
-
+    mutex = Lock()
     while True:
-        state = env.reset()
+        threads = []
+        steps = []
+        simulation_num = 500
         history = []
-        action = RL.action(state)
-        while True:
-            # print(state)
-            env.render()
+        print("sim_start")
+        for _ in range(simulation_num):
+            steps.append(0)
 
-            before_state = state
-            before_action = action
+        for index in range(simulation_num):
+            step = 0
+            thread = Thread(target=simulation, args=(index, mutex, history, steps, RL))
+            thread.start()
+            threads.append(thread)
 
-            state, reward, done, info = env.step(action)
-            action = RL.action(state)
-            history.append([before_state, before_action, state, action, reward, done])
-            
-            if done:
-                break
-        
-        RL.learn(history)
 
+        for thread in threads:
+            thread.join()
+
+        N = 0
+        avg_step = 0
+        for step in steps:
+            avg_step += step
+            N += 1
+
+        avg_step /= N
+        print("avg_step {}".format(avg_step))
+        RL.learn(history, num = 500)
 
 if __name__ == "__main__":
     main()
